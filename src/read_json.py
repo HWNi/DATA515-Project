92a8bef78f529a4348843cae21cf737b93029a5c
Zicong Liang <lzctony@gmail.com>
2017-05-29 20:01:24 -0700

    second version of the read_json function. Obtain dict for nyc's neighborhoods & coordinates

--- src/read_json.py
+++ src/read_json.py
@@ -3,13 +3,13 @@ import json
 def read_json(filename):
 
     """
-    A function read the json file and process the Zipcodes and Coordinates
+    A function read the json file and process the Neighborhoods and Coordinates
 
     Parameter: filename, a json file)
 
     Return: result, a dictionary contains
-            key: zipcode,
-            value: a list of coordinate corresponding to the that zipcode
+            key: neighborhood,
+            value: a list of coordinate corresponding to the that neighborhood
     """
 
     with open(filename) as f:
@@ -17,9 +17,7 @@ def read_json(filename):
 
     result = {}
     for feature in data['features']:
-        result[feature['properties']['ZIPCODE']] = feature['geometry']['coordinates']
-
-    for zipcode, coor in result.items():
-        print("Zipcode {}, Coordinate {}".format(zipcode, coor))
+        # NATName = Neighborhood Tabulation Area Name
+        result[feature['properties']['NTAName']] = feature['geometry']['coordinates']
 
     return result
