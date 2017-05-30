948eb4050c810a5d33143775b5bc5f26c39aea7f
Zicong Liang <lzctony@gmail.com>
2017-05-29 22:17:21 -0700

    third version of read_json function

--- src/read_json.py
+++ src/read_json.py
@@ -1,14 +1,14 @@
 import json
 
-def read_json(filename):
 
+def read_json(filename):
     """
     A function read the json file and process the Neighborhoods and Coordinates
 
     Parameter: filename, a json file)
 
     Return: result, a dictionary contains
-            key: neighborhood,
+            key: Neighborhood,
             value: a list of coordinate corresponding to the that neighborhood
     """
 
@@ -17,7 +17,16 @@ def read_json(filename):
 
     result = {}
     for feature in data['features']:
+
         # NATName = Neighborhood Tabulation Area Name
-        result[feature['properties']['NTAName']] = feature['geometry']['coordinates']
+
+        neighborhood = feature['properties']['NTAName']
+        coordinates = feature['geometry']['coordinates'][0]  # 3 layers or 2 layers
+
+        if len(coordinates[0]) > 2:
+            # 3 layers
+            coordinates = coordinates[0]
+
+        result[neighborhood] = coordinates
 
     return result
