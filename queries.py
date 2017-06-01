f93d3df806c1ed4175c4bcbb1b7c41fc9a3b638c
yawen32 <liy44@uw.edu>
2017-05-31 23:20:52 -0700

    prepare data

--- queries.py
+++ queries.py
@@ -7,31 +7,32 @@ SELECT COUNT(*) FROM [dbo].[apr_time] WHERE [day] = ?? and [hour] = ?? and [minu
 #SELECT COUNT(*) FROM [dbo].[apr_time] WHERE [month] = ??
 
 
-def count_by_query(month,day,hour,minute):
+def count_by_query(month,hour):
 	'''
 	month: the corresponding table name. i.e. apr
 	day: int
 	hour: int
 	minute: int
 	'''
-	prepare_statement = "SELECT distinct [lat], [lon], [neighborhood] FROM [dbo].[" + month + "_time] WHERE [day] = "+ str(day) + "and [hour] = " 
-	+ str(hour) + "and [minute] =" + str(minute) + "group by [neighborhood]"
+	prepare_statement = "SELECT count(*) as count, [neighborhood] FROM [dbo].[" + month + "_time] WHERE [hour] = " 
+	+ str(hour) + "group by [neighborhood]"
 	cursor.execute(prepare_statement)
 	rows = cursor.fetchone()
-	lat_list = []
-	lon_list = []
+	#lat_list = []
+	#lon_list = []
 	neighborhood_list = []
+	count_list = []
 	for row in rows:
-		lat_list.append(row.LAT)
-		lon_list.append(row.LON)
+		#lat_list.append(row.LAT)
+		#lon_list.append(row.LON)
 		neighborhood.append(row.neighborhood)
+		count_list.append(row.count)
 
-	dics1 = {}
-	keys1 = [i for i in neighborhood_list]
-	dics2 = {}
-	keys2 = ["Lat","Lon"]
-
-	for 
-	for key in keys1:
+	dics = {}
+	keys = [i for i in neighborhood_list]
 
 
+	for key in keys:
+		dics[i] = count_list[i]
+
+	return dics
