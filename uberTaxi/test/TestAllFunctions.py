55e062aeb9e13cc63570ea2f425837ea26c4eddd
Zicong Liang <lzctony@gmail.com>
2017-06-07 01:05:58 -0700

    Final Version of Unittest Class

--- uberTaxi/test/TestAllFunctions.py
+++ uberTaxi/test/TestAllFunctions.py
@@ -12,17 +12,41 @@ class TestAllFunctions(unittest.TestCase):
     A unit test class to test our methods.
     """
 
+    # a function test split_data method
     def test_split_data(self):
         self.assertTrue(split_uber_data("test_uber.csv"), True) # fake file to test the function
         self.assertTrue(split_taxi_data("test_taxi.csv"), True) # fake file to test the function
 
+    # a function test read_json method
     def test_read_json(self):
-        self.assertEqual(read_json("NYC_Shapes.json"), read_json("NYC_Shapes.json"))
+        """
+        read_json function returns a dictionary with
+        Key: neighborhood
+        Value: list of (latitude, longitude)
 
+        We should expect to have 195 neighborhood in New York City
+        """
+
+        result = read_json("NYC_Shapes.json")
+        neighborhood = [result.keys()]
+        self.assertEqual(len(neighborhood), 195)
+
+    # a function test process_coordinates method
     def test_process_coordinates(self):
-        self.assertEqual(process_coordinates("NYC_Shapes_Cleaned.json"),
-                         process_coordinates("NYC_Shapes_Cleaned.json"))
+        """
+        process_coordinate function return a dictionary with
+        Key: neighborhood
+        Value: a inner dictionary { 'Lat' : [...], 'Lon' : [...] }
 
+        For example polygon = { 'Airport' : { 'Lat' : [...], 'Lon' : [...] }, .... }
+
+        We should expect the type of polygon is equal to the type of polygon['Airport']
+        """
+
+        polygons = process_coordinates("NYC_Shapes_Cleaned.json")
+        self.assertEqual(type(polygons), type(polygons['Airport']))
+
+    # a function test geo_forward and geo_reverse methods
     def test_geo_convert(self):
         api_key = "Apz4N0shxHlg9r_WGJBPvMl-oi8F26rnkGFsJXa7QlDlSa7CbzWcnAPMVtW3-JHJ"
         address = "4326 University Way NE, Seattle, WA 98105"
@@ -31,10 +55,12 @@ class TestAllFunctions(unittest.TestCase):
         zipcode = geo_reverse(latitude, longitude, api_key)
         self.assertEqual(zipcode, 98105)
 
+    # a function test find_neighborhood method
     def test_find_neighborhood(self):
         result = {'Airport':[[1, 2], [2, 3]]} # fake dictionary to test the function
         self.assertTrue(find_neighborhood(result, "test.csv"))
 
+    # a function test point_inside_polygon method
     def test_point_inside_polygon(self):
         self.assertTrue(point_inside_polygon(4, 3, [5, 5])) # fake points to test the function
         self.assertFalse(point_inside_polygon(6, 5, [5, 5])) # fake points to test the function
